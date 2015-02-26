9938647e08626c1b4086d1c99acf598a72e02c46
Kevin S. Hahn <kevinshahn@gmail.com>
2015-02-26 15:30:57 -0800

    update add_host to use site_id instead of hostname

--- add_host.py
+++ add_host.py
@@ -10,7 +10,7 @@ import datetime
 logging.basicConfig()
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('hostname', help='hostname to be added to authd instances')
+arg_parser.add_argument('site_id', help='site_id to be added to authd instances')
 arg_parser.add_argument('--db_uri', help='DB uri', default='mongodb://127.0.0.1/central')
 args = arg_parser.parse_args()
 
@@ -20,9 +20,9 @@ kwargs = dict(tz_aware=True)
 db_client = pymongo.MongoReplicaSetClient(args.db_uri, **kwargs) if 'replicaSet' in args.db_uri else pymongo.MongoClient(args.db_uri, **kwargs)
 db = db_client.get_default_database()
 
-# is there already a host entry for this hostname?
-if not db.instances.find_one({'_id': args.hostname}):
-    db.instances.insert({'_id': args.hostname, 'date_added': datetime.datetime.now()})
-    log.info('entry created for host %s' % args.hostname)
+# is there already a host entry for this site_id?
+if not db.instances.find_one({'_id': args.site_id}):
+    db.instances.insert({'_id': args.site_id, 'date_added': datetime.datetime.now()})
+    log.info('entry created for host %s' % args.site_id)
 else:
-    log.info('entry already exists for host %s' % args.hostname)
+    log.info('entry already exists for host %s' % args.site_id)
